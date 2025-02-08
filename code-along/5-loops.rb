864463180f43699258928ea2aaeeefb51a3a3023
McKenna Malvitz <mckenna.malvitz@kellogg.northwestern.edu>
2025-02-08 21:01:57 +0000

    completed lab 5

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -6,6 +6,38 @@
 # loop do
 #   puts "tacos!"
 # end
+counter = 0
+loop do
+  if counter == 5
+    break
+  end
+  puts "looping 5 times"
+  counter = counter + 1
+end
 
+friends = ['Rachel', 'Monica', 'Phoebe', 'Ross', 'Chandler', 'Joey']
+
+index = 0
+loop do
+  if index == friends.length
+    break
+  end
+
+  friend = friends[index]
+  greeting = "Hi #{friend}"
+  #puts greeting
+
+  index = index + 1
+end
+
+for friend in friends
+    greeting = "Hi #{friend}"
+    puts greeting
+  end
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+for item in tacos
+    name_item = "I love #{item}"
+    puts name_item
+end
--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -19,6 +19,22 @@ spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 # King of Spades
 # Ace of Spades
 
+#for card in clubs
+#    puts "#{card} of Clubs"
+#end
+
+#for card in diamonds
+#    puts "#{card} of Diamonds"
+#end
+
+#for card in hearts
+#    puts "#{card} of Hearts"
+#end
+
+#for card in spades
+#    puts "#{card} of Spades"
+#end
+
 # CHALLENGE #1
 # The arrays are identical for each suit and can be simplified by using
 # a ranks array as seen below. Try to complete the exercise again by
@@ -26,6 +42,47 @@ spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+for suit in suits
+    for card in ranks
+        puts "#{card} of #{suit}"
+    end
+end
+
 # CHALLENGE #2
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+card_1_rank = ranks.sample
+card_1_suit = suits.sample
+puts "#{card_1_rank} of #{card_1_suit}"
+
+card_2_rank = ranks.sample
+card_2_suit = suits.sample
+puts "#{card_2_rank} of #{card_2_suit}"
+
+card_3_rank = ranks.sample
+card_3_suit = suits.sample
+puts "#{card_3_rank} of #{card_3_suit}"
+
+card_4_rank = ranks.sample
+card_4_suit = suits.sample
+puts "#{card_4_rank} of #{card_4_suit}"
+
+card_5_rank = ranks.sample
+card_5_suit = suits.sample
+puts "#{card_5_rank} of #{card_5_suit}"
+
+
+deck = []
+for rank in ranks
+  for suit in suits
+    card = "#{rank} of #{suit}"
+    deck.push(card)
+  end
+end
+
+shuffled_deck = deck.shuffle
+hand = shuffled_deck[0,5]
+
+for card in hand
+  puts card
+end
